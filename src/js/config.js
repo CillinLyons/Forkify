916a83c0e3edcb8d4df9b675fabdf00362471fc8
CillinLyons <cillinlyons@outlook.com>
2022-07-12 16:43:22 +0100

    Project Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const API_KEY = '2d10cc40-b5f6-4d9a-8764-f7ee6b01747b';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const MODAL_CLOSE_SEC = 2.5;
